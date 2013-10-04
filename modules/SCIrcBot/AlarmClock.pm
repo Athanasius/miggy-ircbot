2e2716d1c1a34bcd31c51f4a9556d4b25420e66d
Athan SC <sc@miggy.org>
2013-10-04 15:41:34 +0100

    More work on AlarmClock, sitll no real functionality

--- modules/SCIrcBot/AlarmClock.pm
+++ modules/SCIrcBot/AlarmClock.pm
@@ -6,6 +6,17 @@ use POE;
 use POE::Component::IRC::Plugin qw(:ALL);
 use POSIX;
 
+my %alarms = {
+  'wmh' => {
+    'fullname' => "Wingman's Hangar Reminder",
+    'day_of_week' => 'Fri',
+    'time' => '11:00:00',
+    'pre_announce_time' => '15', # Minutes
+    'pre_announce_text' => "Wingman's Hangar starts in 15 minutes: http://twitch.tv/roberts_space_ind_ch_1",
+    'timezone' => 'CST6CDT'
+  },
+{;
+
 sub new {
   my ($class, %args) = @_;
 	my $self = bless {}, $class;
