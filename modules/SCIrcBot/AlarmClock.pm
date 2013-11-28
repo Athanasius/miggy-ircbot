7398ab0dd8cfc53c8c752e2c53160952516e09a6
Athan SC <sc@miggy.org>
2013-11-28 14:30:23 +0000

    AlarmClock: tweaks due to no WMH 29th Nov 2013 and livestream etc

--- modules/SCIrcBot/AlarmClock.pm
+++ modules/SCIrcBot/AlarmClock.pm
@@ -13,30 +13,21 @@ use Data::Dumper;
 use constant { SEC => 0, MIN => 1, HOUR => 2, MDAY => 3, MON => 4, YEAR => 5, WDAY => 6, YDAY => 7, ISDST => 8, };
 
 my %alarms = (
-  'wmh' => {
+  'wmh29nov' => {
+    'fullname' => "Wingman's Hangar Reminder",
+    'announce_text' => "There is no Wingman's Hangar this week (29th Nov 2013)",
+    'time' => 'Fri Nov 29 2013 11:00:00',
+    'timezone' => 'CST6CDT',
+    'pre_announce_times' => [1440, 300, 180, 120, 60, 30, 15, 5], # Minutes
+    'pre_announce_text' => "There is no Wingman's Hangar this week (29th Nov 2013) in %s !",
+  },
+  'wmh06dec' => {
     'fullname' => "Wingman's Hangar Reminder",
     'announce_text' => "Wingman's hangar is starting now! http://twitch.tv/roberts_space_ind_ch_1",
-    'time' => 'Fri 11:00:00',
+    'time' => 'Fri Dec  6 2013 11:00:00',
     'timezone' => 'CST6CDT',
-    'pre_announce_times' => [180, 120, 60, 30, 15, 5, 1], # Minutes
-#    'pre_announce_times' => [2,1], # Minutes
+    'pre_announce_times' => [180, 120, 60, 30, 15, 5], # Minutes
     'pre_announce_text' => "Wingman's Hangar starts in %s: http://twitch.tv/roberts_space_ind_ch_1",
-  },
-  '1yrstream' => {
-    'fullname' => "1 Year Anniversay Stream",
-    'announce_text' => "1 year anniversay stream should be starting now! http://www.youtube.com/watch?v=O8HvfFCysYU",
-    'time' => 'Thu Oct 10 2013 18:30:00',
-    'timezone' => 'CST6CDT',
-    'pre_announce_times' => [300, 180, 120, 60, 45, 30, 15, 5, 1], # Minutes
-    'pre_announce_text' => "1 year anniversay stream should be starting in %s http://www.youtube.com/watch?v=O8HvfFCysYU",
-  },
-  '2ndyearstart' => {
-    'fullname' => "Start of 2nd Year Stream",
-    'announce_text' => "Live Stream for start of 2nd year should be starting now! http://twitch.tv/roberts_space_ind_ch_1",
-    'time' => 'Tue Nov 26 2013 11:00:00',
-    'timezone' => 'PST8PDT',
-    'pre_announce_times' => [300, 180, 120, 60, 45, 30, 15, 5, 1], # Minutes
-    'pre_announce_text' => "Live Stream for start of 2nd year should be starting in %s http://twitch.tv/roberts_space_ind_ch_1",
   },
 );
 
