ce29456660338e09a969f3c161f343aa6f5c4342
Athanasius <code@miggy.org>
2016-01-22 18:09:18 +0000

    Need to do different URL retrieval per site, not just different parsing. Youtube was coming along well, but will need to use the proper API:
    <https://developers.google.com/youtube/v3/docs/videos/list>

--- modules/MiggyIRCBot/URLParse.pm
+++ modules/MiggyIRCBot/URLParse.pm
@@ -11,7 +11,8 @@ use HTTP::Request;
 use HTML::TreeBuilder;
 
 my %sites = (
-  'imgur\.com$' => \&parse_imgur_com
+  'imgur\.com$' => {get => \&get_generic, parse => \&parse_imgur_com},
+  '^www\.youtube\.com$' => {get => \&get_youtube_com, parse => \&parse_youtube_com}
 );
 
 sub new {
@@ -37,7 +38,7 @@ sub PCI_register {
   }
   $self->{session_id} = POE::Session->create(
     object_states => [
-      $self => [ qw(_shutdown _start _get_url _parse_url ) ],
+      $self => [ qw(_shutdown _start _get_url get_generic _parse_url ) ],
     ],
   )->ID();
   $poe_kernel->state( 'get_url', $self );
@@ -77,6 +78,39 @@ sub get_url {
 sub _get_url {
   my ($kernel, $self) = @_[KERNEL, OBJECT];
   my %args;
+printf STDERR "_GET_URL\n";
+  if ( ref $_[ARG0] eq 'HASH' ) {
+     %args = %{ $_[ARG0] };
+  } else {
+     %args = @_[ARG0..$#_];
+  }
+  $args{lc $_} = delete $args{$_} for grep { !/^_/ } keys %args;
+
+printf STDERR "_GET_URL: URL '%s'\n", $args{'url'};
+  my ($host) = $args{'url'} =~ /^http[s]?:\/\/([^\/:]+)(:[0-9]+)?\//;
+printf STDERR "_GET_URL: Host '%s'\n", $host;
+  my $done;
+  foreach my $site (keys(%sites)) {
+printf STDERR "_GET_URL: Checking site '%s'\n", $site;
+    if ($host =~ $site) {
+printf STDERR "_GET_URL: Recognised a %s site...\n"; #\t%s\n", $site, Dumper(${sites}{$site});
+      $sites{$site}->{'get'}->($kernel, $self, \%args);
+      $done = 1;
+      last;
+    }
+  }
+
+  if (! $done) {
+#printf STDERR "_GET_URL: posting to http_alias\n";
+    $kernel->post( $self->{session_id}, 'get_generic', @_[ARG0..$#_] );
+  }
+
+  undef;
+}
+
+sub get_generic {
+  my ($kernel, $self) = @_[KERNEL, OBJECT];
+  my %args;
 #printf STDERR "_GET_URL\n";
   if ( ref $_[ARG0] eq 'HASH' ) {
      %args = %{ $_[ARG0] };
@@ -86,9 +120,7 @@ sub _get_url {
   $args{lc $_} = delete $args{$_} for grep { !/^_/ } keys %args;
 
   my $req = HTTP::Request->new('GET', $args{'url'});
-#printf STDERR "_GET_URL: posting to http_alias\n";
   $kernel->post( $self->{http_alias}, 'request', '_parse_url', $req, \%args );
-  undef;
 }
 
 sub _parse_url {
@@ -118,8 +150,8 @@ printf STDERR "_PARSE_URL: args->{url} = '%s'\n", $args->{'url'};
     my $done;
     foreach my $site (keys(%sites)) {
       if ($host =~ $site) {
-printf STDERR "_PARSE_URL: Recgonised a %s site...\n", $site;
-        my $blurb = $sites{$site}->($res, $args);
+printf STDERR "_PARSE_URL: Recognised a %s site...\n", $site;
+        my $blurb = $sites{$site}->{'parse'}($res, $args);
         if (defined($blurb)) {
           push @params, 'irc_miggybot_url_success', $args, $blurb;
         } else {
@@ -160,6 +192,84 @@ sub parse_imgur_com {
 
 # imgur.com is a PITA, fills in <title> etc after the fact with javascript
 # waste of time to respond with the generic page title
+## 16:25:35 <bigp3rm-> NASA just released an image of the new planet 168:http://imgur.com/yfTAqXq
+## 16:25:36 <EDBot> [ New images of Planet 9 worrying for scientists. - Imgur ] - imgur.com
   return "";
 }
+
+sub get_youtube_com {
+  my ($kernel, $self, $args) = @_;
+
+#printf STDERR "\t%s\n", Dumper($kernel);
+  $kernel->post( $self->{session_id}, 'get_generic', $args );
+}
+
+# 21:02:28<EDBot> [YouTube] Title: The Good, The Bad and The Bucky: Highlights from the Buckyball Run | Uploader: Esvandiary | Uploaded: 2015-05-15 00:47:44 UTC | Duration: 00:07:13 | Views: 1,591 | Comments: 7 | Likes: 39 | Dislikes: 1
+sub parse_youtube_com {
+  my ($res, $args) = @_;
+
+  my $blurb = "";
+  if ($res->header('Content-Type') =~ /^text\/(ht|x)ml/) {
+    my $tree = HTML::TreeBuilder->new;
+    $tree->parse($res->decoded_content);
+    $tree->eof();
+    my $title = $tree->look_down('_tag', 'title');
+    if (! $title) {
+      return $blurb;
+    }
+    $blurb = "[ YouTube ] Title: " . $title->as_text;
+
+    my $yt_user_info = $tree->look_down('_tag' => 'div', 'class' => 'yt-user-info');
+    if ($yt_user_info) {
+      my $a = $yt_user_info->look_down('_tag' => 'a');
+      if ($a) {
+        $blurb .= " | Uploader: " . $a->as_text;
+      }
+    }
+
+    my $datePublished = $tree->look_down('_tag' => 'meta', 'itemprop' => 'datePublished');
+    if ($datePublished) {
+      $blurb .= " | Uploaded: " . $datePublished->attr('content');
+    }
+
+    my $duration = $tree->look_down('_tag' => 'meta', 'itemprop' => 'duration');
+    if ($duration) {
+      my $d = $duration->attr('content');
+      my ($min, $sec, $hour) = $d =~ /^PT([0-9]+)M([0-9]+)S$/;
+      if ($min >= 60) {
+        $hour = int($min / 60);
+        $min -= $hour * 60;
+      } else {
+        $hour = 0;
+      }
+      $blurb .= sprintf " | Duration: %02d:%02d:%02d", $hour, $min, $sec;
+    }
+
+    my $interactionCount = $tree->look_down('_tag' => 'meta', 'itemprop' => 'interactionCount');
+    if ($interactionCount) {
+      $blurb .= " | Views: " . prettyprint($interactionCount->attr('content'));
+    }
+
+    if ($title) {
+      return $blurb;
+    } else {
+      return undef;
+    }
+  # } elsif (image) {
+  } else {
+    $args->{'quiet'} = 1;
+    return "That was not an HTML page";
+  }
+  return undef;
+}
+
+sub prettyprint {
+  my $number = sprintf "%.0f", shift @_;
+  # Add one comma each time through the do-nothing loop
+  1 while $number =~ s/^(-?\d+)(\d\d\d)/$1,$2/;
+  # Put the dollar sign in the right place
+  #$number =~ s/^(-?)/$1\$/;
+  $number;
+}
+
 1;
