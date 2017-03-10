9ca0b89601e756d6f794eda2cc96de7ea468f539
Athanasius <code@miggy.org>
2017-03-10 23:58:29 +0000

    Correctly regex new GUID URLs, as the RSS feed has already stripped the embedded title

--- miggy-ircbot.pl
+++ miggy-ircbot.pl
@@ -289,7 +289,7 @@ sub irc_miggybot_rss_newitems {
 #printf STDERR "IRC_MIGGYBOT_RSS_NEWITEMS: Item with permaLink '%s'\n", $i->{'permaLink'};
       # https://forums.frontier.co.uk/showthread.php?t=101801&p=3541911#post3541911
       if ($i->{'permaLink'} =~ /http(s)?:\/\/forums\.frontier\.co\.uk\/showthread\.php\?t=(?<t>[0-9]+)(&p=(?<p>[0-9]+)#post(?<post>[0-9]+))?$/
-        or $i->{'permaLink'} =~ /http(s)?:\/\/forums\.frontier\.co\.uk\/showthread\.php\/(?<t>[0-9]+)-.+(\?p=(?<p>[0-9]+)#post(?<post>[0-9]+))?$/) {
+        or $i->{'permaLink'} =~ /http(s)?:\/\/forums\.frontier\.co\.uk\/showthread\.php\/(?<t>[0-9]+)(-.+)?(\?p=(?<p>[0-9]+)#post(?<post>[0-9]+))?$/) {
 #printf STDERR "IRC_MIGGYBOT_RSS_NEWITEMS: Item has a Frontier Forums permaLink\n";
         my $post;
         if (defined($+{'p'})) {
